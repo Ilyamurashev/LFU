d826f1d8ee0187e27fc5e6a263deb1a9cf70f7f0
Илья Мурашев <ilya.murashev178@gmail.com>
2023-05-20 02:33:01 +0300

    forgot to erase the structures prototypes and extra comments

--- If_in_and_cleaner.c
+++ If_in_and_cleaner.c
@@ -1,39 +1,7 @@
-#include <stdlib.h>
 
-// IN: the number of frequency = key, hash table of pages, lfu to get the size
-// OUT: 1/0 equals true or false
+//returns pointer to a page
 
-
-struct freq_node {               //structure of frequency node
-    int value;                   //frequency
-    struct freq_node *next;
-    struct freq_node *prev;
-};
-
-struct page {
-    struct freq_node *parent;    //freq of  page
-    struct page *next;           //next     page in list of freq_node
-    struct page *prev;           //previous page in list of freq_node
-    int value;                   //value of page
-    struct page *next_hash;      //next     page in bucket of hash_t
-    struct page *prev_hash;      //previous page in bucket of hash_t
-};
-
-struct bucket_freq_node {        //element of frequency hash table, index = frequency
-    struct page *last;           //last page  in list of freq_node
-    struct page *first;          //first page in list of freq_node
-    int length;                  //length   of   list of freq_node
-};
-
-struct lfu_cache {
-    struct page **hash_t;
-    struct freq_node *freq_head;
-    int size_hash_t;             //size of hash table
-    int size_freq_hash_t;        //size of frequency hash table
-};
-
-//returns pointer to circle
-struct if_page_exist(int key, struct page **hash_t){
+struct page* if_page_exist(int key, struct page **hash_t){
 
   int index = get_page_hash(key);
   struct page *node = hash_t[index];
