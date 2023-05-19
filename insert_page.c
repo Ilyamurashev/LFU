5ff2d1dcbeefe60a4bf07d83cab0fe46696d69e9
OnTon1 <novomlinov.as@phystech.edu>
2023-05-19 18:57:34 +0300

    insert_func

--- insert_page.c
+++ insert_page.c
@@ -17,18 +17,18 @@ struct page {
 struct page *insert_page_in_hash_t(int key, struct page **hash_t){
     int id = 0;
     id = get_page_hash(key);
-    struct page *ball;
+    struct page *new_page;
     struct page *tp;
     tp = hash_t[id]->next;
-    ball = (struct page *)calloc(1, sizeof(struct page));
+    new_page = (struct page *)calloc(1, sizeof(struct page));
     if(tp == NULL) tp = hash_t[id];
     //Insert
     while(tp->next != NULL){
         tp = tp->next;
         }
-    tp->next = ball;      //
-    ball->next = NULL;    //bandaging
-    ball->prev = tp;      //
+    tp->next = new_page;      //
+    new_page->next = NULL;    //bandaging
+    new_page->prev = tp;      //
 
-    return ball;
+    return page;
 }
