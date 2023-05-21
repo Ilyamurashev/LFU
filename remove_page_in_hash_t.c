978382e70a20d4b8e2f5b4e1a66e0ece9e37d3bb
OnTon1 <novomlinov.as@phystech.edu>
2023-05-21 16:27:09 +0300

    now the desired page is being deleted

--- remove_page_in_hash_t.c
+++ remove_page_in_hash_t.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <assert.h>
 
 struct page {
     struct freq_node *parent;
@@ -9,6 +10,11 @@ struct page {
     struct page *prev_hash;
 };
 
+int check_key(int key, struct page* page){
+    if(key == page->value) return 1;
+    else return 0;
+}
+
 //This function remove the page
 
 void remove_page_in_hash_t(int key, struct page **hash_t){
@@ -16,13 +22,35 @@ void remove_page_in_hash_t(int key, struct page **hash_t){
     id = get_hash(key);
     struct page *tp;
     struct page *prev;
+    struct page *next;
     tp = hash_t[id];
-    while(tp->next != NULL){    //
-        prev = tp;             // looking for the last element
-        tp = tp->next;        //
+    assert(tp != NULL);
+    next = tp->next_hash;
+    if(check_key(key, tp) == 1){                    //
+        free(tp);                                   // key == value on the first circle
+        if(next != NULL) next->prev_hash = NULL;    //
+    }
+    else{
+        prev = tp;
+        tp = next;
+        next = tp->next_hash;
+        while(check_key(key, tp) != 1){
+            prev = tp;                                           //
+            tp = tp->next_hash;                                 // looking for the key
+            if(next != NULL) next = tp->next_hash;             //
         }
-    prev->next = NULL;      // remove page
-    free(tp);              //
+        if(next == NULL){                                    // 
+            prev->next_hash = next;                         // the key is in the last circle
+            free(tp);                                      //
+        }
+        else{                                            // 
+            prev->next_hash = next;                     // the key is in the middle
+            next->prev_hash = prev;                    //
+            free(tp);
+        }
+    }
+
+          
     
 
     
